e12e8c6d5efea147967b1e74d68cad39c5095162
emcog-solutions <jairam.250204@gmail.com>
2026-02-06 23:01:40 +0530

    initial

---
+++ fourwheelbot/Core/Src/main.c
@@ -0,0 +1,602 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "FreeRTOS.h"
+#include "task.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+#define DEVICE_ADDR (0x27<<1)
+uint8_t command1[]={0x30,0x30,0x30,0x20,'\0'};
+uint8_t command[]={0x28,0x0E,0x01,0x07,0x8F,'\0'};
+uint8_t lcd_data[] ="RTOS-Program";
+uint8_t lcd_data1[]="EGS-PILLAY";
+int lcd_button_flag=0;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim3;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+
+void lcd_init(void);
+void lcd_write_command1(uint8_t command_1);
+void lcd_write_command(uint8_t data);
+void lcd_write_data(uint8_t data);
+void delay();
+
+void motor_task(void *parameter);
+void lcd_task(void *parameter);
+void lcdbutton_task(void *parameter);
+void emergency_task(void *parameter);
+
+TaskHandle_t motor_handle;
+TaskHandle_t lcd_handle;
+TaskHandle_t lcdbutton_handle;
+TaskHandle_t emergency_handle;
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  MX_I2C1_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+
+
+
+
+  xTaskCreate(motor_task, "motor", 100, NULL, 3, &motor_handle);
+  xTaskCreate(lcd_task, "lcd", 200, NULL, 2, &lcd_handle);
+  xTaskCreate(lcdbutton_task, "lcdbutton", 50, NULL, 2, &lcdbutton_handle);
+
+  xTaskCreate(emergency_task, "emergency", 50, NULL, 4, &emergency_handle);
+
+  vTaskStartScheduler();
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 8-1;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 1000-1;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, Air_bag_Pin|emergency_led_Pin|in1_Pin|in2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, in3_Pin|in4_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : Air_bag_Pin emergency_led_Pin in1_Pin in2_Pin */
+  GPIO_InitStruct.Pin = Air_bag_Pin|emergency_led_Pin|in1_Pin|in2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : lcdbutton_Pin */
+  GPIO_InitStruct.Pin = lcdbutton_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(lcdbutton_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : in3_Pin in4_Pin */
+  GPIO_InitStruct.Pin = in3_Pin|in4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+
+void motor_task(void *parameter){
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+
+	while(1){
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 0);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, 1);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, 0);
+		TIM3->CCR2=700;
+		TIM3->CCR4=700;
+		vTaskDelay(500);
+	}
+}
+
+void lcd_task(void *parameter){
+	int i=0;
+	 lcd_init();
+	while(1){
+		while(lcd_button_flag==0){
+			lcd_write_command(0x01);
+			lcd_write_command(0x8F);
+			for(i=0;i<12&&lcd_button_flag==0;i++){
+				lcd_write_data(lcd_data[i]);
+				vTaskDelay(200);
+			}
+		}
+		while(lcd_button_flag==1){
+			lcd_write_command(0x01);
+			lcd_write_command(0x8F);
+			for(i=0;i<10&&lcd_button_flag==1;i++){
+				lcd_write_data(lcd_data1[i]);
+				vTaskDelay(200);
+			}
+		}
+	  HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13);
+	  vTaskDelay(500);
+	}
+}
+void lcdbutton_task(void *parameter){
+	while(1){
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4)==0){
+			lcd_button_flag=0;
+		}
+		else{
+			lcd_button_flag=1;
+		}
+		vTaskDelay(100);
+	}
+}
+
+void emergency_task(void *parameter){
+	while(1){
+		xTaskNotifyWait(0,0,NULL,portMAX_DELAY);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 1);
+		vTaskSuspend(lcd_handle);
+		vTaskSuspend(lcdbutton_handle);
+		vTaskSuspend(motor_handle);
+		HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
+		HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_4);
+		while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5)==0){
+			HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+			vTaskDelay(100);
+		}
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 0);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 0);
+		vTaskResume(lcd_handle);
+		vTaskResume(lcdbutton_handle);
+		vTaskResume(motor_handle);
+		HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+		HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+
+	}
+}
+
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
+
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+  BaseType_t xHigherPriorityTaskWoken;
+
+xTaskNotifyFromISR(emergency_handle,0,eNoAction,&xHigherPriorityTaskWoken);
+portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+
+
+void lcd_init(){
+	uint8_t i=0;
+	//while(command[i]!='\0'){
+	//	lcd_write_command(command[i]);
+	//	i++;
+	//}
+	for(i=0;i<4;i++){
+			lcd_write_command1(command1[i]);
+
+		}
+	for(i=0;i<6;i++){
+		lcd_write_command(command[i]);
+		HAL_Delay(100);
+
+	}
+}
+
+void lcd_write_command1(uint8_t command_1){
+	uint8_t upper_nibble = command_1&0xF0;
+	uint8_t backlight = 0x08;
+	uint8_t en = 0x04;
+	uint8_t rw = 0x02;
+	uint8_t rs = 0x01;
+	uint8_t packet = (upper_nibble | backlight | en) & ~(rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet, 1, 10);
+	HAL_Delay(1);
+	packet = (upper_nibble | backlight) & ~(en | rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet, 1, 10);
+	HAL_Delay(5);
+}
+
+void lcd_write_command(uint8_t data){
+	uint8_t upper_nibble = data&0xF0;
+	uint8_t lower_nibble = (data<<4)&0xF0;
+	uint8_t backlight = 0x08;
+	uint8_t en = 0x04;
+	uint8_t rw = 0x02;
+	uint8_t rs = 0x01;
+
+	//uint8_t packet1 = upper_nibble | backlight ;
+	//packet1 = packet1 | en ;
+	//packet1 = packet1 &(~(rw|rs))  ;
+	uint8_t packet1 = (upper_nibble | backlight | en ) & ~(rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+	packet1 = (upper_nibble | backlight ) & ~(en | rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+	uint8_t packet2 = (lower_nibble | backlight | en ) & ~(rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+	packet2 = (lower_nibble | backlight ) & ~(en | rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+
+}
+
+void lcd_write_data(uint8_t data){
+	uint8_t upper_nibble = data&0xF0;
+	uint8_t lower_nibble = (data<<4)&0xF0;
+	uint8_t backlight = 0x08;
+	uint8_t en = 0x04;
+	uint8_t rw = 0x02;
+	uint8_t rs = 0x01;
+
+	//uint8_t packet1 = upper_nibble | backlight | rs ;
+	//packet1 = packet1 | en ;
+	//packet1 = packet1 &(~rw)  ;
+	uint8_t packet1 = (upper_nibble | backlight | en | rs) & ~(rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+	//packet1 = packet1 &(~en)  ;
+	packet1 = (upper_nibble | backlight | rs) & ~(en | rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+
+	uint8_t packet2 = (lower_nibble | backlight | en | rs) & ~(rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+	packet2 = (lower_nibble | backlight | rs) & ~(en | rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
